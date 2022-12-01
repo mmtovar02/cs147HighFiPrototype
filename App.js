428f156bd6e194d4ff65d359ccb0ce2fae252f0a
Maeghan Tovar <maeghantovar@gmail.com>
2022-12-01 00:22:47 -0800

    creates components for homescreen

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, ScrollView, View, Image } from 'react-native';
 import {useFonts, WorkSans_400Regular, WorkSans_500Medium} from '@expo-google-fonts/work-sans'
 
 import Task from "./components/Task.js"
+import WelcomeBanner from './components/WelcomeBanner.js';
 
 export default function App() {
   let [fontsLoaded] = useFonts({
@@ -84,89 +85,11 @@ export default function App() {
       marginRight: 16,
     },
 
-    task: {
-      marginBottom: 8,
-      paddingLeft: 24,
-      paddingRight: 24,
-      flexDirection: 'row', 
-      justifyContent: 'space-between',
-      borderWidth: 2,
-      borderColor: '#fff1ef',
-      borderRadius: 24, 
-    },
-
-    taskTimeText: {
-      color: '#828997',
-      fontSize: 14,
-      marginTop: 16
-    },
-
-    taskTitleText: {
-      fontFamily: 'WorkSans_500Medium',
-      fontSize: 20,
-      marginTop: 8,
-    },
-
-    taskProfileContainer: {
-      flexDirection: 'row', 
-      marginTop: 8, 
-      marginBottom: 16,
-    },
-
-    taskProfileImage: {
-      height: 32,
-      width: 32,
-      resizeMode: 'contain',
-      borderRadius: 100, 
-      marginRight: 8,
-    },
-    
-    taskIcon: {
-      height: 30,
-      width: 30,
-      resizeMode: 'contain',
-      borderRadius: 100, 
-      alignSelf: 'center'
-    },
-
-    taskIconBackground: {
-      backgroundColor:'#fff1ef',
-      height: 60,
-      width: 60,
-      justifyContent: 'center',
-      borderRadius: 60,
-      alignSelf: 'center',
-    }
   });
 
   return (
     <ScrollView style={styles.container}>
-      <View style={styles.welcomeBanner}> 
-        <Text style={styles.welcomeTitle}>Welcome, Sarah</Text>
-        <Text style={styles.welcomeSubtitle}>How are you feeling?</Text>
-        <View style={styles.welcomeEmoticonsContainer}>
-          <View>
-            <Image style={styles.welcomeEmoticonsImages} source={require('./assets/HeartEmoticons/amazing.png')}></Image>
-            <Text style={styles.welcomeEmoticonText}>Amazing</Text>
-          </View>
-          <View>
-            <Image style={styles.welcomeEmoticonsImages} source={require('./assets/HeartEmoticons/good.png')}></Image>
-            <Text style={styles.welcomeEmoticonText}>Good</Text>
-          </View>
-          <View>
-            <Image style={styles.welcomeEmoticonsImages} source={require('./assets/HeartEmoticons/neutral.png')}></Image>
-            <Text style={styles.welcomeEmoticonText}>Neutral</Text>
-          </View>
-          <View>
-            <Image style={styles.welcomeEmoticonsImages} source={require('./assets/HeartEmoticons/bad.png')}></Image>
-            <Text style={styles.welcomeEmoticonText}>Bad</Text>
-          </View>
-          <View>
-            <Image style={styles.welcomeEmoticonsImages} source={require('./assets/HeartEmoticons/awful.png')}></Image>
-            <Text style={styles.welcomeEmoticonText}>Awful</Text>
-          </View>
-        </View>
-      </View>
+      <WelcomeBanner/>
       <Text style={styles.homeTitle}>Today</Text>
       <View style={styles.taskContainer}>
         <Task title="Meditate" time="8:00 PM" icon={require('./assets/TaskIcons/meditate.png')}/>
---
+++ components/EmoticonButton.js
@@ -0,0 +1,48 @@
+import { StyleSheet, Text, View, Image } from 'react-native';
+import {useFonts, WorkSans_400Regular} from '@expo-google-fonts/work-sans'
+
+export default function EmoticonButton(props) {
+    let [fontsLoaded] = useFonts({
+        WorkSans_400Regular,
+    });
+    
+    if (!fontsLoaded) { 
+        return null;
+    }
+
+    const styles = StyleSheet.create({
+        emoticonIcon: {
+            height: 40,
+            width: 40,
+            resizeMode: 'contain',
+            alignSelf: 'center'
+        },
+  
+        emoticonBackground: {
+            height: 60,
+            width: 60,
+            borderRadius: 60,
+            borderColor: '#fff',
+            borderWidth: 4,
+            justifyContent: 'center',
+            alignSelf: 'center',
+        },
+      
+        emoticonText: {
+            fontFamily: 'WorkSans_400Regular',
+            fontSize: 12,
+            textAlign: 'center',
+            marginTop: 8,
+            marginBottom: 32,
+        },
+    });
+
+    return (
+        <View>
+            <View style={styles.emoticonBackground}>
+                <Image style={styles.emoticonIcon} source={props.icon}></Image>
+            </View>
+            <Text style={styles.emoticonText}>{props.label}</Text>
+        </View>
+    );  
+}
