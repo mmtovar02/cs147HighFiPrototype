3acaad92bb3215a3d7812615b643a2e7c6cade82
Maeghan Tovar <mtovar@DN0a22c1ee.SUNet>
2022-12-09 10:14:24 -0800

    fixes Back button navigation Conversation

--- screens/Facilitate/Conversation.js
+++ screens/Facilitate/Conversation.js
@@ -1,76 +1,96 @@
-import { useState, useEffect } from 'react';
-import { StyleSheet, Image, Text, View, ScrollView, TextInput, KeyboardAvoidingView, Platform, Keyboard, TouchableOpacity } from 'react-native';
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
-import { useFonts, WorkSans_400Regular, WorkSans_500Medium }from '@expo-google-fonts/work-sans';
+import { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Image,
+  Text,
+  View,
+  ScrollView,
+  TextInput,
+  KeyboardAvoidingView,
+  Platform,
+  Keyboard,
+  TouchableOpacity,
+} from "react-native";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import {
+  useFonts,
+  WorkSans_400Regular,
+  WorkSans_500Medium,
+} from "@expo-google-fonts/work-sans";
 
-import Header from '../../components/Header.js'
-import CallAndVideoButtons from '../../components/CallAndVideoButtons.js';
-import MessageStream from '../../components/MessageStream.js';
-import Message from '../../components/Message.js';
-import MessageTextInput from '../../components/MessageTextInput.js';
-import DiscussionTopic from '../../components/DiscussionTopic.js';
+import Header from "../../components/Header.js";
+import CallAndVideoButtons from "../../components/CallAndVideoButtons.js";
+import MessageStream from "../../components/MessageStream.js";
+import Message from "../../components/Message.js";
+import MessageTextInput from "../../components/MessageTextInput.js";
+import DiscussionTopic from "../../components/DiscussionTopic.js";
 
 export default function Conversation({ navigation, route }) {
-    const [messageSent, setMessageSent] = useState(false);
-    const [ref, setRef] = useState(null);
+  const [messageSent, setMessageSent] = useState(false);
+  const [ref, setRef] = useState(null);
 
-    let [fontsLoaded] = useFonts({
-        WorkSans_400Regular,
-        WorkSans_500Medium, 
-    });
+  let [fontsLoaded] = useFonts({
+    WorkSans_400Regular,
+    WorkSans_500Medium,
+  });
 
-    if (!fontsLoaded) { 
-        return null;
-    }
+  if (!fontsLoaded) {
+    return null;
+  }
 
-    let { discussionTopic } = route.params ? route.params : { discussionTopic: false };
+  let { discussionTopic } = route.params
+    ? route.params
+    : { discussionTopic: false };
 
-    return (
-        <View style={styles.container}>
-            <Header 
-                title='Cole, Lily' 
-                facilitatorSubtitle={true}
-                rightButton={<CallAndVideoButtons onVideoPress={() => {}}/>}  
-                includeBackArrow={true} 
-                onBackArrowPress={() => navigation.goBack()}
-            />
-            <KeyboardAwareScrollView 
-                style={{marginBottom: 60}}
-                ref={ref => {setRef(ref)}}
-                onContentSizeChange={() => ref.scrollToEnd({animated: true})}
-            >
-                <MessageStream />
-                {discussionTopic ?
-                    <Message 
-                        message="What are you doing this week to take care of yourself?"
-                        label='Me'
-                        user={true}
-                        image={require('../../assets/ProfilePictures/user.jpeg')}
-                        removeImage={messageSent}
-                    />
-                    :
-                    ""
-                }
-                {messageSent ?
-                    <Message 
-                        message="How are you all doing?"
-                        user={true}
-                        label={!discussionTopic ? 'Me' : ''}
-                        image={require('../../assets/ProfilePictures/user.jpeg')}
-                    />
-                    :
-                    ""
-                } 
-            </KeyboardAwareScrollView>
-            <MessageTextInput setMessageSent={setMessageSent}/>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Header
+        title="Cole, Lily"
+        facilitatorSubtitle={true}
+        rightButton={<CallAndVideoButtons onVideoPress={() => {}} />}
+        includeBackArrow={true}
+        onBackArrowPress={() =>
+          navigation.navigate("Inbox", { askState: false })
+        }
+      />
+      <KeyboardAwareScrollView
+        style={{ marginBottom: 60 }}
+        ref={(ref) => {
+          setRef(ref);
+        }}
+        onContentSizeChange={() => ref.scrollToEnd({ animated: true })}
+      >
+        <MessageStream />
+        {discussionTopic ? (
+          <Message
+            message="What are you doing this week to take care of yourself?"
+            label="Me"
+            user={true}
+            image={require("../../assets/ProfilePictures/user.jpeg")}
+            removeImage={messageSent}
+          />
+        ) : (
+          ""
+        )}
+        {messageSent ? (
+          <Message
+            message="How are you all doing?"
+            user={true}
+            label={!discussionTopic ? "Me" : ""}
+            image={require("../../assets/ProfilePictures/user.jpeg")}
+          />
+        ) : (
+          ""
+        )}
+      </KeyboardAwareScrollView>
+      <MessageTextInput setMessageSent={setMessageSent} />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#ffff'
-    },
-
+  container: {
+    flex: 1,
+    backgroundColor: "#ffff",
+  },
 });
--- screens/Facilitate/Inbox.js
+++ screens/Facilitate/Inbox.js
@@ -1,92 +1,112 @@
-import { useState } from 'react';
-import { StyleSheet, View, ScrollView, Image, TouchableOpacity } from 'react-native';
+import { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+} from "react-native";
 
-import Header from '../../components/Header.js'
-import DiscussionTopic from '../../components/DiscussionTopic.js'
-import ConversationBlock from '../../components/ConversationBlock.js'
+import Header from "../../components/Header.js";
+import DiscussionTopic from "../../components/DiscussionTopic.js";
+import ConversationBlock from "../../components/ConversationBlock.js";
 
-export default function Inbox({ navigation }) {
-    const [askState, setAskState] = useState(false);
+export default function Inbox({ navigation, route }) {
+  const [askState, setAskState] = useState(false);
 
-    const getHeader = () => {
-        return askState ?
-            (<Header 
-                title='' 
-                includeBackArrow={true}
-                onBackArrowPress={() => setAskState(!askState)}
-                rightButtonLabel='Send'
-                onRightButtonPress={() => navigation.navigate('Conversation', { discussionTopic: true})}
-            />)
-            :
-            (<Header 
-                title='Inbox' 
-                rightButton={
-                    <TouchableOpacity>
-                        <Image source={require('../../assets/NavigationIcons/newMessage.png')} style={styles.newMessageIcon}/>
-                    </TouchableOpacity>
-                }
-            />);
-    }
+  if (route.params && route.params.askState != askState) {
+    setAskState(route.params.askState);
+    route.params = false;
+  }
 
-    return (
-        <View style={styles.container}>
-            {getHeader()}
-            <ScrollView>
-                <DiscussionTopic displayAskButton={!askState} onAskButtonPress={() => setAskState(!askState)}/>
-                <View style={styles.conversationContainer}>
-                    <ConversationBlock
-                        label='Cole, Lily' 
-                        lastMessage="Cole: I don't have that... &#x2022; Fri"
-                        facilitatorReminder="3 days since last message. Check in?"
-                        facilitator={true}
-                        notification={!askState}
-                        selected={askState}
-                        onPress={() => navigation.navigate('Conversation')}
-                        image1={require('../../assets/ProfilePictures/lily.jpeg')} 
-                        image2={require('../../assets/ProfilePictures/cole.jpeg')}
-                    />
-                    <ConversationBlock
-                        label='Mom, Dad' 
-                        lastMessage="Mom: I see &#x2022; 4:43 pm"
-                        grayed={askState}
-                        image1={require('../../assets/ProfilePictures/dad.jpeg')} 
-                        image2={require('../../assets/ProfilePictures/mom.jpeg')}
-                    />
-                    <ConversationBlock
-                        label='Cole' 
-                        lastMessage="Cole: That's awesome! &#x2022; 12:03 pm"
-                        grayed={askState}
-                        image1={require('../../assets/ProfilePictures/cole.jpeg')} 
-                    />
-                    <ConversationBlock
-                        label='Lily' 
-                        lastMessage="Me: That's a good question... &#x2022; Sun"
-                        grayed={askState}
-                        image1={require('../../assets/ProfilePictures/lily.jpeg')} 
-                    />
-                </View>
-            </ScrollView>
-        </View>
+  const getHeader = () => {
+    return askState ? (
+      <Header
+        title=""
+        includeBackArrow={true}
+        onBackArrowPress={() => setAskState(!askState)}
+        rightButtonLabel="Send"
+        onRightButtonPress={() =>
+          navigation.navigate("Conversation", { discussionTopic: true })
+        }
+      />
+    ) : (
+      <Header
+        title="Inbox"
+        rightButton={
+          <TouchableOpacity>
+            <Image
+              source={require("../../assets/NavigationIcons/newMessage.png")}
+              style={styles.newMessageIcon}
+            />
+          </TouchableOpacity>
+        }
+      />
     );
+  };
+
+  return (
+    <View style={styles.container}>
+      {getHeader()}
+      <ScrollView>
+        <DiscussionTopic
+          displayAskButton={!askState}
+          onAskButtonPress={() => setAskState(!askState)}
+        />
+        <View style={styles.conversationContainer}>
+          <ConversationBlock
+            label="Cole, Lily"
+            lastMessage="Cole: I don't have that... &#x2022; Fri"
+            facilitatorReminder="3 days since last message. Check in?"
+            facilitator={true}
+            notification={!askState}
+            selected={askState}
+            onPress={() => navigation.navigate("Conversation")}
+            image1={require("../../assets/ProfilePictures/lily.jpeg")}
+            image2={require("../../assets/ProfilePictures/cole.jpeg")}
+          />
+          <ConversationBlock
+            label="Mom, Dad"
+            lastMessage="Mom: I see &#x2022; 4:43 pm"
+            grayed={askState}
+            image1={require("../../assets/ProfilePictures/dad.jpeg")}
+            image2={require("../../assets/ProfilePictures/mom.jpeg")}
+          />
+          <ConversationBlock
+            label="Cole"
+            lastMessage="Cole: That's awesome! &#x2022; 12:03 pm"
+            grayed={askState}
+            image1={require("../../assets/ProfilePictures/cole.jpeg")}
+          />
+          <ConversationBlock
+            label="Lily"
+            lastMessage="Me: That's a good question... &#x2022; Sun"
+            grayed={askState}
+            image1={require("../../assets/ProfilePictures/lily.jpeg")}
+          />
+        </View>
+      </ScrollView>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#ffff'
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#ffff",
+  },
 
-    conversationContainer: {
-        backgroundColor: '#fff',
-        marginLeft: 16,
-        marginRight: 16,
-        marginBottom: 64,
-    },
+  conversationContainer: {
+    backgroundColor: "#fff",
+    marginLeft: 16,
+    marginRight: 16,
+    marginBottom: 64,
+  },
 
-    newMessageIcon: {
-        height: 24,
-        width: 24,
-        resizeMode: 'contain',
-        alignSelf: 'center',
-    }
+  newMessageIcon: {
+    height: 24,
+    width: 24,
+    resizeMode: "contain",
+    alignSelf: "center",
+  },
 });
